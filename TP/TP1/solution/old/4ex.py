5af2aad619767ef41785af7783633da6f3bbe7a7
partizanos <dimi.proios@gmail.com>
2019-05-03 01:33:04 +0200

    modified

--- TP/TP1/solution/old/4ex.py
+++
@@ -1,5 +0,0 @@
-# Read the image peppers.png and convert it to grayscale and add Gaussian noise
-# N (0, 625). Perform its low-rank approximation for k = 1, ..., n. Plot the dependence between
-# the k and MSE of k-rank approximation version of original image. Make a conclusion.
-
-print("4ex.py")
--- TP/TP1/solution/old/ex1_2.py
+++
@@ -1,6 +0,0 @@
-# function [ X ] = Ex_1_2_WGN( dim_n, dim_m, variance_val, mean_val )
-#
-# a = randn(dim_n, dim_m);
-# X = mean_val + (sqrt(variance_val) * (a-mean(a(:))) / std(a(:)));
-#
-# end
